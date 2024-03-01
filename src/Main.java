95558cab15a5edf85602d7dcc5ea5dbb327be2c9
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-03-01 20:02:03 +0700

    [IMPROVE] impprove kode program

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
         int angka = input.nextInt();
 
         System.out.println("--- Panjang ---");
